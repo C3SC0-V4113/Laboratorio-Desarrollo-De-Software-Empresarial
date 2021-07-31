ee105eca2eb1ec59762a09d3edd520ac912ee9d6
Francisco Valle <vallecesco@gmail.com>
2021-07-31 11:14:22 -0600

    Terminado los Modelos de Pelicula

---
+++ Guia 1 Introducción a ASP.NET MVC 5 Parte 1/Ejemplo 1/MvcPelicula/MvcPelicula/Models/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcPelicula.Models
+{
+    public class Pelicula
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public string Genero { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
