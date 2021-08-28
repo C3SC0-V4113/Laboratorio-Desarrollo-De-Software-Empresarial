dbf610316be9a14528984e9624d091e0ee0d4d57
CESCO-VALLE <cescovalledev@gmail.com>
2021-08-28 08:59:08 -0600

    A la mitad de guia 2

---
+++ Guia 2 Intro a ASP.NET Parte 2/Ejemplo 1/MvcPelicula/Controllers/PeliculasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcPelicula.Models;
+
+namespace MvcPelicula.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private PeliculaDBContext db = new PeliculaDBContext();
+
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            return View(db.Peliculas.ToList());
+        }
+
+        // GET: Peliculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Titulo,FechaLanzamiento,Genero,Precio")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peliculas.Add(pelicula);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Titulo,FechaLanzamiento,Genero,Precio")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pelicula).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pelicula pelicula = db.Peliculas.Find(id);
+            db.Peliculas.Remove(pelicula);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
