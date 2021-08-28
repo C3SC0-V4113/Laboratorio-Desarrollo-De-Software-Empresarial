48bc6b0d70cca0252f7b50f9e8a6b8c8c05718d9
CESCO-VALLE <cescovalledev@gmail.com>
2021-08-28 07:43:46 -0600

    Comenzando Guia-2

---
+++ Guia 2 Intro a ASP.NET Parte 2/Ejercicio 1/MvcPelicula/MvcPelicula/Models/Pelicula.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcPelicula.Models
+{
+    public class Pelicula
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public string Genero { get; set; }
+        public decimal Precio { get; set; }
+    }
+
+    public class PeliculaDBContext : DbContext
+    {
+        public DbSet<Pelicula> Peliculas { get; set; }
+    }
+}
