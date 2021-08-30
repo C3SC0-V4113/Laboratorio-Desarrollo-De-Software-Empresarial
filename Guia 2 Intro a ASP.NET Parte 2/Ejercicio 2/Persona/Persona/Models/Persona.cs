937bc4c0cb6ecb745be4ec09d17cfe9cd3e2daf6
Francisco Valle <vallecesco@gmail.com>
2021-08-29 21:51:49 -0600

    Empezando Ejercicio 2

---
+++ Guia 2 Intro a ASP.NET Parte 2/Ejercicio 2/Persona/Persona/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        public int DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNac { get; set; }
+        public string Direccion { get; set; }
+        public string CorreoE { get; set; }
+    }
+    public class PersonaDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
