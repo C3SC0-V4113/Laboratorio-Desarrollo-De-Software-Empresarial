f503bf509d6ad570c6269b74f4e5315bc8736faf
Francisco Valle <vallecesco@gmail.com>
2021-10-01 23:43:16 -0600

    Trabajando en el ejemplo

--- Guia 4 Capas/capas/src/datos/contacto.java
+++ Guia 4 Capas/capas/src/datos/contacto.java
@@ -1,5 +1,28 @@
 package datos;
 
 public class contacto {
-
+	private String nombre;
+	private long celular;
+	
+	public contacto(String nombre, long celular) {
+		super();
+		this.nombre = nombre;
+		this.celular = celular;
+	}
+	@Override
+	public String toString() {
+		return "contacto [nombre=" + nombre + ", celular=" + celular + "]";
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public long getCelular() {
+		return celular;
+	}
+	public void setCelular(long celular) {
+		this.celular = celular;
+	}
 }
